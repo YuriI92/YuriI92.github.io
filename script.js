0642a797aae1c1a0864d6b9d1a1c4256e0fe0b2c
Yurippe <yurichikawa1992@gmail.com>
2022-03-29 19:01:35 -0400

    modify js file

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-var apiKey = '4da80e5512c9db1543dad0a071ed0134';
-// var redirect_url = 'http://musicslayers.com';
+var client_id = 'bf8c5db27c99417e921ce6b9739bf1a5';
+var redirect_uri = 'https://slayersband.com/callback/';
 
 var scope = 'user-read-private user-read-email';
 
-var url = 'http://www.last.fm/api/auth/?api_key=' + apiKey;
-//  + '&cb=' + redirect_url;
+var url = 'https://accounts.spotify.com/authorize';
+url += '?response_type=token';
+url += '&client_id=' + encodeURIComponent(client_id);
+url += '&scope=' + encodeURIComponent(scope);
+url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
 
-fetch(url, {
-    headers: {Authentication: 'Bearer Token'}
-  })
+fetch(url)
     .then(function(response) {
         console.log(response);
     });
